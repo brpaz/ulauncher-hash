fd6a92c24f58385eba396b368e583beeca6bc2c9
Bruno Paz <brunopaz@sapo.pt>
2018-06-02 14:05:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Ulauncher hash
+
+> [ulauncher](https://ulauncher.io/) Extension to generate hashes from a string, using common algoriths like md5, sha1, sha256 ..
+
+## Usage
+
+![demo](demo.gif)
+
+
+## Requirements
+
+* [ulauncher](https://ulauncher.io/)
+* Python >= 2.7
+
+## Install
+
+Open ulauncher preferences window -> extensions -> add extension and paste the following url:
+
+```https://github.com/brpaz/ulauncher-hash```
+ 
+
+## Development
+
+```
+git clone https://github.com/brpaz/ulauncher-hash
+cd ~/.cache/ulauncher_cache/extensions/ulauncher-hash
+ln -s <repo_location> ulauncher-hash
+```
+
+To see your changes, stop ulauncher and run it from the command line with: ```ulauncher -v```.
+
+## License 
+
+MIT
---
+++ main.py
@@ -0,0 +1,74 @@
+import logging
+import hashlib
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+
+logger = logging.getLogger(__name__)
+
+class HashExtension(Extension):
+
+    def __init__(self):
+        logger.info('init hash Extension')
+        super(HashExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+   
+        md5Hash = hashlib.md5(event.get_argument()).hexdigest()
+        sha1Hash = hashlib.sha1(event.get_argument()).hexdigest()
+        sha224Hash = hashlib.sha224(event.get_argument()).hexdigest()
+        sha256Hash = hashlib.sha256(event.get_argument()).hexdigest()
+        sha512Hash = hashlib.sha512(event.get_argument()).hexdigest()
+        
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name=md5Hash,
+                                         description='md5',
+                                         highlightable=False,
+                                         on_enter=CopyToClipboardAction(
+                                             md5Hash)
+                                         ))
+
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                          name=sha1Hash,
+                                          description='sha1',
+                                          highlightable=False,
+                                          on_enter=CopyToClipboardAction(
+                                              sha1Hash)
+                                          ))
+
+
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                          name=sha224Hash,
+                                         description='sha224',
+                                         highlightable=False,
+                                         on_enter=CopyToClipboardAction(
+                                             sha224Hash)
+                                         ))
+        
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                          name=sha256Hash,
+                                          description='sha256',
+                                          highlightable=False,
+                                          on_enter=CopyToClipboardAction(
+                                              sha256Hash)
+                                          ))
+
+        items.append(ExtensionResultItem(icon='images/icon.png',
+                                         name=sha512Hash,
+                                         description='sha512',
+                                         highlightable=False,
+                                         on_enter=CopyToClipboardAction(
+                                              sha512Hash)
+                                         ))
+
+        return RenderResultListAction(items)
+
+if __name__ == '__main__':
+   HashExtension().run()
